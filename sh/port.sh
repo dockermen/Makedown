3053fd8bfb78ba1c84915ddd7e7670c3436d1dde
zhilong0224 <85558580+zhilong0224@users.noreply.github.com>
2021-06-27 23:44:41 +0800

    Create port.sh

---
+++ sh/port.sh
@@ -0,0 +1,21 @@
+a=$1
+b=$2
+if [ "$a"x == "start"x ]
+then
+   firewall-cmd --zone=public --add-port=$b/tcp --permanent
+   echo "开启端口:$b成功"
+   firewall-cmd --reload
+   echo '重启防火墙'
+elif [ "$a"x == "stop"x ]
+then
+   firewall-cmd --zone=public --remove-port=$b/tcp --permanent
+   echo "关闭端口:$b成功"
+   firewall-cmd --reload
+   echo '重启防火墙'
+elif [ "$a"x == "ps"x ]
+then
+   echo "当前端口情况"
+   firewall-cmd --zone=public --list-ports
+else
+   echo "没有传递参数"
+fi
